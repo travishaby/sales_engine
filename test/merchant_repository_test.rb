51ec19a576735d6eb7d0d0dff8ab57cdc50e77ae
Travis Haby <travishaby@gmail.com>
2015-08-03 19:15:22 -0600

    finished merchant class, wrote more tests for merchant repo

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -15,30 +15,32 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_loading_merchants_manually
-    skip
     merchant_repo = MerchantRepository.new
-    values = ["merchant name", "created_at", "updated_at"]
-    assert_equal values,
-    merchant_repo.add_merchant("1", Merchant.new("id", "name", "created_at", "updated_at"))
+    merch1 = Merchant.new("1", "name", "created_at", "updated_at")
+    assert_equal merch1,
+    merchant_repo.add_merchant("1", merch1)
   end
 
   def test_returning_expected_hash_after_adding_merchants
     merchant_repo = MerchantRepository.new
-    values = ["merchant name", "created_at", "updated_at"]
-
-
-    assert_equal values, merchant_repo.add_merchant("1", Merchant.new("1", "name", "created_at", "updated_at"))
-
-    assert_equal values, merchant_repo.add_merchant("2", Merchant.new("id", "name", "created_at", "updated_at"))
-
-    expected_hash = {"1"=>["merchant name", "created_at", "updated_at"],
-    "2"=>["merchant name", "created_at", "updated_at"]}
+    merch1 = Merchant.new("1", "name", "created_at", "updated_at")
+    merch2 = Merchant.new("id", "name", "created_at", "updated_at")
+    assert_equal merch1, merchant_repo.add_merchant("1", merch1)
+    assert_equal merch2, merchant_repo.add_merchant("2", merch2)
+    expected_hash = {"1"=>merch1, "2"=>merch2}
     assert_equal expected_hash, merchant_repo.merchants
   end
 
   def test_that_csv_values_are_imported_from_file
     merchant_repo = MerchantRepository.new
     assert_equal merchant_repo.merchants, merchant_repo.load_merchants
+  end
+
+  def test_accessing_merchant_name_with_id
+    merchant_repo = MerchantRepository.new
+    merchant_repo.load_merchants
+    assert_equal :"Schroeder-Jerde", merchant_repo.merchants["1"].name
+
   end
 
 end
