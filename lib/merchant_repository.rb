3bc3717586bfc8f7f83e3a52a47e2d920590e375
MB Burch <marybethburch@gmail.com>
2015-08-06 15:03:40 -0600

    16 added list and search to merchant repo and merchant repo test

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -5,13 +5,68 @@ class MerchantRepository
 
   attr_reader :merchants, :merchant_file
 
-  def initialize(merchant_file = './data/merchants.csv')
+  def initialize(engine = nil, merchant_file = './data/merchants.csv')
     @merchants = {}
     @merchant_file = merchant_file
+    @engine = engine
   end
 
   def merchant_loader
     MerchantLoader.new(self)
+  end
+
+  def all
+    merchants.values
+  end
+
+  def random
+    merchants.values.to_a.sample
+  end
+
+  def find_by_id(id)
+    merchants[id]
+  end
+
+  def find_by(attribute, match)
+    found = merchants.detect do |id, object|
+      object.send(attribute).downcase == match.downcase
+    end
+    found || empty = []
+  end
+
+  def find_by_name(name)
+    find_by(:name, name)
+  end
+
+  def find_by_created_at(created_at)
+    find_by(:created_at, created_at)
+  end
+
+  def find_by_updated_at(updated_at)
+    find_by(:updated_at, updated_at)
+  end
+
+  def find_by_all(attribute, match)
+    found = merchants.select do |id, object|
+      object.send(attribute).downcase == match.downcase
+    end
+    found || empty = []
+  end
+
+  def find_all_by_id(id)
+    find_by_all(:id, id)
+  end
+
+  def find_all_by_name(name)
+    find_by_all(:name, name)
+  end
+
+  def find_all_by_created_at(created_at)
+    find_by_all(:created_at, created_at)
+  end
+
+  def find_all_by_updated_at(updated_at)
+    find_by_all(:updated_at, updated_at)
   end
 
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -53,4 +53,89 @@ class MerchantRepositoryTest < Minitest::Test
     refute data_setup.merchants["1001"]
   end
 
+
+    def test_that_all_returns_all_instances
+      assert_equal 100, fixture_setup.all.size
+    end
+
+    def test_random_usually_returns_different_merchant
+      merch1 = fixture_setup.random
+      merch2 = fixture_setup.random
+      5.times do
+        assert merch1 != merch2
+        merch2 = fixture_setup.random
+      end
+    end
+
+    def test_general_find_by_method_with_updated_at
+      assert_equal "2012-03-27 14:54:09 UTC",
+      fixture_setup.find_by(:updated_at,"2012-03-27 14:54:09 UTC")[1].updated_at
+    end
+
+    def test_returns_single_object_with_matching_id
+      assert_equal "Fahey-Stiedemann", fixture_setup.find_by_id("99").name
+    end
+
+    def test_returns_single_object_with_matching_name
+      assert_equal "Fahey-Stiedemann", fixture_setup.find_by_name("Fahey-Stiedemann")[1].name
+    end
+
+    def test_returns_single_object_with_case_insensitive_name
+      assert_equal "Fahey-Stiedemann", fixture_setup.find_by_name("fahey-stiedemann")[1].name
+    end
+
+    def test_returns_single_object_with_matching_created_at
+      assert_equal "2012-03-27 14:54:00 UTC",
+      fixture_setup.find_by_created_at("2012-03-27 14:54:00 UTC")[1].created_at
+    end
+
+    def test_returns_single_object_with_matching_updated_at
+      assert_equal "2012-03-27 14:54:00 UTC",
+      fixture_setup.find_by_updated_at("2012-03-27 14:54:00 UTC")[1].updated_at
+    end
+
+    def test_general_find_by_all_method_with_name_and_specific_keys
+      assert_equal ["5", "6"],
+      fixture_setup.find_by_all(:name,"Williamson Group").keys
+    end
+
+    def test_general_find_by_all_method_with_name
+      assert_equal 2,
+      fixture_setup.find_by_all(:name,"Williamson Group").size
+    end
+
+    def test_general_find_by_all_returns_empty_array_if_name_not_found
+      assert_equal [],
+      fixture_setup.find_by_all(:name,"Burch").keys
+    end
+
+    def test_general_find_by_all_returns_array_size_zero_if_name_not_found
+      assert_equal 0,
+      fixture_setup.find_by_all(:name,"Burch").size
+    end
+
+    def test_find_all_by_id
+      assert_equal 1, fixture_setup.find_all_by_id("76").size
+    end
+
+    def test_find_all_by_name
+      assert_equal 2, fixture_setup.find_all_by_name("Williamson Group").size
+    end
+
+    def test_find_all_by_name_with_specific_keys
+      assert_equal ["5", "6"], fixture_setup.find_all_by_name("Williamson Group").keys
+    end
+
+    def test_find_all_by_name_case_insensitive
+      assert_equal ["5", "6"], fixture_setup.find_all_by_name("williamson Group").keys
+    end
+
+    def test_find_all_by_created_at
+      assert_equal 9, fixture_setup.find_all_by_created_at("2012-03-27 14:53:59 UTC").size
+    end
+
+    def test_find_all_by_updated_at
+      assert_equal 8, fixture_setup.find_all_by_updated_at("2012-03-27 14:53:59 UTC").size
+    end
+
 end
