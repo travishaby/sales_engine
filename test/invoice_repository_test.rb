d228bbe74219681cbf2a853c71bde85e0c831312
MB Burch <marybethburch@gmail.com>
2015-08-06 11:52:43 -0600

    added data and fixture setup to invoice repo test

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -3,12 +3,17 @@ require './lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
 
-  # def fixture_setup
-  #   invoice_repository = InvoiceRepository.new('./fixtures/invoices.csv')
-  #   invoice_repository.invoice_loader.load_invoices
-  #   invoice_repository
-  # end
+  def data_setup
+    invoice_repository = InvoiceRepository.new
+    invoice_repository.invoice_loader.load_invoices
+    invoice_repository
+  end
 
+  def fixture_setup
+    invoice_repository = InvoiceRepository.new(nil, './fixtures/invoices.csv')
+    invoice_repository.invoice_loader.load_invoices
+    invoice_repository
+  end
 
   def test_repo_starts_as_empty_hash
     invoice_repository = InvoiceRepository.new
@@ -27,42 +32,30 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_accesses_customer_id_with_id
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
-    assert_equal "1", invoice_repository.invoices["1"].customer_id
+    assert_equal "1", data_setup.invoices["1"].customer_id
   end
 
   def test_accesses_merchant_id_with_id
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
-    assert_equal "75", invoice_repository.invoices["2"].merchant_id
+    assert_equal "75", data_setup.invoices["2"].merchant_id
   end
 
   def test_accesses_status_with_id
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
     assert_equal "shipped",
-    invoice_repository.invoices["3"].status
+    data_setup.invoices["3"].status
   end
 
   def test_accesses_invoice_created_at_with_id
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
     assert_equal "2012-03-24 15:54:10 UTC",
-    invoice_repository.invoices["4"].created_at
+    data_setup.invoices["4"].created_at
   end
 
   def test_accesses_invoice_updated_at_with_id
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
     assert_equal "2012-03-24 15:54:10 UTC",
-    invoice_repository.invoices["4"].updated_at
+    data_setup.invoices["4"].updated_at
   end
 
   def test_attempt_access_nonexistent_value
-    invoice_repository = InvoiceRepository.new
-    invoice_repository.invoice_loader.load_invoices
-    refute invoice_repository.invoices["5000"]
+    refute data_setup.invoices["5000"]
   end
 
 end
