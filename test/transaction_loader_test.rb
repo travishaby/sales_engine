90e43f4148e07c580140e36b8cee0f140bf4d720
Travis Haby <travishaby@gmail.com>
2015-08-06 15:21:46 -0600

    fixed typo 'transcation' in transaction loader test

--- test/transaction_loader_test.rb
+++ test/transaction_loader_test.rb
@@ -31,27 +31,27 @@ class TransactionLoaderTest < Minitest::Test
   end
 
   def test_that_csv_values_are_imported_from_fixture_file
-    transcation_repository = TransactionRepository.new('./fixtures/transactions.csv')
-    assert_equal transcation_repository.transactions,
-    transcation_repository.transaction_loader.load_transactions
+    transaction_repository = TransactionRepository.new('./fixtures/transactions.csv')
+    assert_equal transaction_repository.transactions,
+    transaction_repository.transaction_loader.load_transactions
   end
 
   def test_accessing_fixture_values
-    transcation_repository = TransactionRepository.new('./fixtures/transactions.csv')
-    transcation_repository.transaction_loader.load_transactions
-    assert_equal "89", transcation_repository.transactions["98"].invoice_id
+    transaction_repository = TransactionRepository.new('./fixtures/transactions.csv')
+    transaction_repository.transaction_loader.load_transactions
+    assert_equal "89", transaction_repository.transactions["98"].invoice_id
   end
 
   def test_that_csv_values_are_imported_from_real_csv_file
-    transcation_repository = TransactionRepository.new
-    assert_equal transcation_repository.transactions,
-    transcation_repository.transaction_loader.load_transactions
+    transaction_repository = TransactionRepository.new
+    assert_equal transaction_repository.transactions,
+    transaction_repository.transaction_loader.load_transactions
   end
 
   def test_accessing_values_from_full_csv_file
-    transcation_repository = TransactionRepository.new
-    transcation_repository.transaction_loader.load_transactions
-    assert_equal "4620443924688732", transcation_repository.transactions["120"].credit_card_number
+    transaction_repository = TransactionRepository.new
+    transaction_repository.transaction_loader.load_transactions
+    assert_equal "4620443924688732", transaction_repository.transactions["120"].credit_card_number
   end
 
 end
